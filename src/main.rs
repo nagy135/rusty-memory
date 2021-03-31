9d1be0eea640752acebbb8f0da3ae1bc975e9bfa
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-03-31 20:28:53 +0200

    feat: body separation from, Struct, Enum

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::char::REPLACEMENT_CHARACTER;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+
+#[derive(Debug)]
+enum Kind {
+    Todo,
+    Note,
+}
+
+#[derive(Debug)]
+struct Message<'a> {
+    kind: Kind,
+    content: &'a str,
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -17,9 +30,16 @@ fn handle_connection(mut stream: TcpStream) {
 
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-    let contents = "Recorded!".to_string();
+    let incoming = String::from_utf8_lossy(&buffer[..]);
+    let body: &str = incoming.split("\r\n\r\n").nth(1).expect("No body sent!!!");
 
+    let message = Message {
+        kind: Kind::Todo,
+        content: &incoming,
+    };
+    println!("Request: {:?}", message);
+    println!("parts: {:?}", body);
+    let contents = "Recorded!".to_string();
     let response = format!(
         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
         contents.len(),
