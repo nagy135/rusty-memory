6e9718b653f4f1ec62b89c3ec8357cf6e20d8c6f
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-01 11:46:20 +0200

    feat: json init + test (buggy now)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+mod json;
 mod server;
 
 fn main() {
     server::create_server();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::json;
+    #[test]
+    fn obj() {
+        assert_eq!(
+            json::parse("{\"key\": \"val\"}"),
+            json::Object {
+                key_vals: vec![Box::new(json::KeyVal {
+                    key: "key",
+                    val: Box::new("val")
+                })]
+            }
+        );
+        ()
+    }
 }
