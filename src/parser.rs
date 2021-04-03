7da2b77b5f022595c87eef41079102b32863a3b4
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-04-03 22:15:06 +0200

    feat: parser ...now only hardcoded 2 KeyVals (abstract this!)

--- src/parser.rs
+++ src/parser.rs
@@ -1,1 +1,71 @@
-pub struct KeyVal;
+use regex::{Captures, Regex};
+
+#[derive(Debug)]
+pub struct KeyVal {
+    pub key: String,
+    pub value: String,
+}
+
+impl KeyVal {
+    pub fn new(key: String, value: String) -> KeyVal {
+        KeyVal { key, value }
+    }
+}
+
+pub fn parse_request<'b>(incoming: String) -> Result<Vec<KeyVal>, &'b str> {
+    // separate header and body {{{
+    let mut request_iter = incoming.split("\r\n\r\n").take(2);
+    let header: &str = match request_iter.next() {
+        Some(header) => header,
+        None => {
+            return Err("Header not found!");
+        }
+    };
+    let body_with_padding: &str = match request_iter.next() {
+        Some(header) => header,
+        None => {
+            return Err("Body not found!");
+        }
+    };
+    // }}}
+
+    // clean up body {{{
+    let re = Regex::new(r"Content-Length: (\d*)").unwrap();
+    let length: Captures = match re.captures_iter(header).next() {
+        Some(length) => length,
+        None => return Err("No Content-Length sent!"),
+    };
+    let length: &str = &length[1];
+    let length: usize = match length.parse() {
+        Ok(length) => length,
+        Err(_) => return Err("NAN Content-Type length"),
+    };
+    let (body, _) = body_with_padding.split_at(length);
+    // }}}
+
+    let mut body_iter = body.split("::").take(2);
+    let message_type: &str = match body_iter.next() {
+        Some(message_type) => message_type,
+        None => return Err("Type not sent! (before ::)"),
+    };
+
+    let mut type_iter = message_type.split(":").take(2);
+    let type_type: &str = match type_iter.next() {
+        Some(type_type) => type_type,
+        None => return Err("Type key not sent!"),
+    };
+    let type_content: &str = match type_iter.next() {
+        Some(type_content) => type_content,
+        None => return Err("Type content not sent!"),
+    };
+
+    let message_content: &str = match body_iter.next() {
+        Some(message_content) => message_content,
+        None => return Err("Content not sent! (after ::)"),
+    };
+
+    Ok(vec![
+        KeyVal::new(type_type.to_string(), type_content.to_string()),
+        KeyVal::new("content".to_string(), message_content.to_string()),
+    ])
+}
--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,8 @@
-use regex::{Captures, Regex};
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
+
+use crate::parser;
 
 const URL: &'static str = "127.0.0.1";
 const PORT: &'static str = "7878";
@@ -39,53 +40,16 @@ fn handle_connection<'a>(mut stream: TcpStream) -> Result<&'a str, &'a str> {
     stream.read(&mut buffer).unwrap();
     let incoming = String::from_utf8_lossy(&buffer[..]);
 
-    // separate header and body {{{
-    let mut request_iter = incoming.split("\r\n\r\n").take(2);
-    let header: &str = match request_iter.next() {
-        Some(header) => header,
-        None => {
-            return Err("Header not found!");
-        }
-    };
-    let body_with_padding: &str = match request_iter.next() {
-        Some(header) => header,
-        None => {
-            return Err("Body not found!");
-        }
-    };
-    // }}}
+    let key_vals: Vec<parser::KeyVal> = parser::parse_request(incoming.into_owned())?;
+    println!("key_vals {:?}", key_vals[0].key);
 
-    // clean up body {{{
-    let re = Regex::new(r"Content-Length: (\d*)").unwrap();
-    let length: Captures = match re.captures_iter(header).next() {
-        Some(length) => length,
-        None => return Err("No Content-Length sent!"),
-    };
-    let length: &str = &length[1];
-    let length: usize = match length.parse() {
-        Ok(length) => length,
-        Err(_) => return Err("NAN Content-Type length"),
-    };
-    let (body, _) = body_with_padding.split_at(length);
-    // }}}
-
-    let mut body_iter = body.split("::").take(2);
-    let message_type: &str = match body_iter.next() {
-        Some(message_type) => message_type,
-        None => return Err("Type not sent! (before ::)"),
-    };
-    let message_content: &str = match body_iter.next() {
-        Some(message_content) => message_content,
-        None => return Err("Content not sent! (after ::)"),
-    };
-
-    let message_kind = match message_type {
-        "type:note" => Kind::Note,
+    let message_kind = match key_vals[0].value.as_str() {
+        "note" => Kind::Note,
         _ => Kind::Todo,
     };
     let message = Message {
         kind: message_kind,
-        content: &message_content,
+        content: &key_vals[1].value,
     };
     println!("message: {:?}", message);
 
